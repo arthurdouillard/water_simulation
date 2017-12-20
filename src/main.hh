a718952d2759448e6a7bd631924ae2640c8497be
Arthur Douillard <douill_a@epita.fr>
2017-12-20 16:35:48 +0100

    Add first draft.

---
+++ src/main.hh
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <cmath>
+
+#include "shader.h"
+#include "camera.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+#endif
