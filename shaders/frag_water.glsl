4d69dc38b264e64f49e5fd9319314ca3762a87cc
Arthur Douillard <douill_a@epita.fr>
2018-01-06 00:17:48 +0100

    Add cubemap reflection to water.

--- shaders/frag_water.glsl
+++ shaders/frag_water.glsl
@@ -11,12 +11,16 @@ uniform vec3 cameraPos;
 void main()
 {
     vec4 tmp = texture(myText, TexCoord);
-    FragColor = vec4(tmp.rgb, 1.0f);
+    FragColor = vec4(tmp.rgb, 0.8f);
+    //FragColor = vec4(0.250, 0.643, 0.874, 0.8);
 
     vec3 Normals = normalize(cross(dFdx(vertexPos), dFdy(vertexPos)));
 
     float ratio = 1.00 / 1.33;
     vec3 I = normalize(vertexPos - cameraPos);
-    vec3 R = refract(I, normalize(-Normals), ratio);
-    FragColor *= vec4(texture(skybox, R).rgb, 1.0);
+    vec3 refraction = refract(I, normalize(-Normals), ratio);
+    FragColor *= vec4(texture(skybox, refraction).rgb, 1.0);
+
+    vec3 reflection = reflect(I, normalize(-Normals));
+    FragColor *= vec4(texture(skybox, reflection).rgb, 1.0);
 }
